b1d78c782ac8ab64428320aad57a788ef4cf0beb
LamarrTheHeadcrab <40360946+LamarrTheHeadcrab@users.noreply.github.com>
2018-06-18 19:09:37 +0500

    Initial commit

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+$db_type = 'mysql';
+$db_host = 'localhost';
+$db_name = 'fluxbb';
+$db_username = 'root';
+$db_password = '';
+$db_prefix = '';
+$p_connect = false;
+
+$cookie_name = 'pun_cookie_c2c66c';
+$cookie_domain = '';
+$cookie_path = '/';
+$cookie_secure = 0;
+$cookie_seed = '41fcb5ef2e2b2fa5';
+
+define('PUN', 1);
